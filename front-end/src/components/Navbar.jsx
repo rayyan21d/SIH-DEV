1ec410e8b432e923d1038d1950b56ac7d6a38005
rayyan21d <rayyan9290@gmail.com>
2023-12-12 19:31:50 +0530

    map component adding

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ const Navbar = () => {
   
     return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      
       <a className="navbar-brand" href="#">Home</a>
-     
       <a className="navbar-brand" href="#">Profile</a>
       <a className="navbar-brand" href="#">One</a>
       <a className="navbar-brand" href="#">Two</a>
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Form from './components/Form.jsx'
 import Navbar from './components/Navbar.jsx'
+import Map from './components/Map.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <>
   <Navbar />
-    <App />
-    <Form />
+  <App />
+  <Form />
+  <Map/ >
+    
   </>
 )
