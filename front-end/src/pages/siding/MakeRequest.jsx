9ff257f81205a7aa0664f633082d062d944f628f
raniafarooq <raniamehreenfarooq18@gmail.com>
2023-12-17 15:12:17 +0530

    Added forms for siding manager

--- front-end/src/pages/siding/MakeRequest.jsx
+++ front-end/src/pages/siding/MakeRequest.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
@@ -9,23 +9,26 @@ import Typography from '@mui/joy/Typography';
 
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
-import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
-
 import Sidebar from './components/Sidebar';
-import OrderTable from './components/OrderTable';
-import OrderList from './components/OrderList';
 import Header from './components/Header';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Stack from '@mui/joy/Stack';
+import Card from '@mui/joy/Card';
+import CardActions from '@mui/joy/CardActions';
+import { useNavigate } from 'react-router-dom';
 
-import {AddForm, UpdateForm, DeleteForm} from './SidingForms';
-// import MyProfile from './MyProfile';
 
 
 export default function MakeRequest() {
+  const navigate = useNavigate();
 
   const [showForm, setShowForm] = useState(false);
 
 
-    const [showAddForm, setShowAddForm] = useState(false);
+  const [showAddForm, setShowAddForm] = useState(false);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
   const [showDeleteForm, setShowDeleteForm] = useState(false);
 
@@ -37,14 +40,14 @@ export default function MakeRequest() {
   }
 
   function handleUpdateClick() {
-     setShowForm(false);
+    setShowForm(false);
     setShowAddForm(false);
     setShowUpdateForm(true);
     setShowDeleteForm(false);
   }
 
   function handleDeleteClick() {
-     setShowForm(false);
+    setShowForm(false);
     setShowAddForm(false);
     setShowUpdateForm(false);
     setShowDeleteForm(true);
@@ -58,195 +61,194 @@ export default function MakeRequest() {
 
 
 
-//   const [values, setValues] = useState({ firstName: '', lastName: '' });
+  //   const [values, setValues] = useState({ firstName: '', lastName: '' });
 
-// function handleChange(event) {
-//   setValues({
-//     ...values,
-//     [event.target.name]: event.target.value
-//   });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // function handleChange(event) {
+  //   setValues({
+  //     ...values,
+  //     [event.target.name]: event.target.value
+  //   });
+  // }
 
   return (
 
     <>
 
-    <CssVarsProvider disableTransitionOnChange>
-      <CssBaseline />
-      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+      <CssVarsProvider disableTransitionOnChange>
+        <CssBaseline />
+        <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
 
-        <Header />
-        <Sidebar />
+          <Header />
+          <Sidebar />
 
-        <Box
-          component="main"
-          className="MainContent"
-          sx={{
-            px: { xs: 2, md: 6 },
-            pt: {
-              xs: 'calc(12px + var(--Header-height))',
-              sm: 'calc(12px + var(--Header-height))',
-              md: 3,
-            },
-            pb: { xs: 2, sm: 2, md: 3 },
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 0,
-            height: '100dvh',
-            gap: 1,
-          }}
-        >
-        {/* The upper nav section */}
-        
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Breadcrumbs
-              size="sm"
-              aria-label="breadcrumbs"
-              separator={<ChevronRightRoundedIcon fontSize="sm" />}
-              sx={{ pl: 0 }}
-            >
-              <Link
-                underline="none"
-                color="neutral"
-                href="#dashboard"
-                aria-label="Home"
-              >
-                <HomeRoundedIcon />
-              </Link>
-              <Link
-                underline="hover"
-                color="neutral"
-                href="#some-link"
-                fontSize={12}
-                fontWeight={500}
-              >
-                Dashboard
-              </Link>
-              <Typography color="primary" fontWeight={500} fontSize={12}>
-                Siding
-              </Typography>
-            </Breadcrumbs>
-          </Box>
-          
-          {/* Manage Siding Heading */}
           <Box
+            component="main"
+            className="MainContent"
             sx={{
+              px: { xs: 2, md: 6 },
+              pt: {
+                xs: 'calc(12px + var(--Header-height))',
+                sm: 'calc(12px + var(--Header-height))',
+                md: 3,
+              },
+              pb: { xs: 2, sm: 2, md: 3 },
+              flex: 1,
               display: 'flex',
-              mb: 1,
+              flexDirection: 'column',
+              minWidth: 0,
+              height: '100dvh',
               gap: 1,
-              flexDirection: { xs: 'column', sm: 'row' },
-              alignItems: { xs: 'start', sm: 'center' },
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
             }}
           >
-            <Typography level="h2" component="h1">
-              Make Request
-            </Typography>
+            {/* The upper nav section */}
 
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Breadcrumbs
+                size="sm"
+                aria-label="breadcrumbs"
+                separator={<ChevronRightRoundedIcon fontSize="sm" />}
+                sx={{ pl: 0 }}
+              >
+                <Link
+                  underline="none"
+                  color="neutral"
+                  href="#dashboard"
+                  aria-label="Home"
+                >
+                  <HomeRoundedIcon />
+                </Link>
+                <Link
+                  underline="hover"
+                  color="neutral"
+                  href="#some-link"
+                  fontSize={12}
+                  fontWeight={500}
+                >
+                  Dashboard
+                </Link>
+                <Typography color="primary" fontWeight={500} fontSize={12}>
+                  Make Request
+                </Typography>
+              </Breadcrumbs>
+            </Box>
 
-           <Box
-           sx={{display:'flex',
-           justifyContent:'space-between', gap:1
-           }}
-           >
-             <Button
-              color="primary"
-              size="sm"
-              id='add'
-              onClick={handleAddClick}
-
+            {/* Manage Siding Heading */}
+            <Box
+              sx={{
+                display: 'flex',
+                mb: 1,
+                gap: 1,
+                flexDirection: { xs: 'column', sm: 'row' },
+                alignItems: { xs: 'start', sm: 'center' },
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+              }}
             >
-              Make new Request
-            </Button>
+              <Typography level="h2" component="h1">
+                Make Request
+              </Typography>
 
 
-              <Button
-              color="primary"
-              id='update'
-              onClick={handleUpdateClick}
 
-              size="sm"
 
-            >
-              Update Request
-            </Button>
 
-              <Button
-              color="primary"
-              size="sm"
-              id='delete'
-              onClick={handleDeleteClick}
 
-            >
-              Cencel Request
-            </Button>
 
-           </Box>
+            </Box>
 
+            {/* The main content of the page */}
+
+            <Card>
+              {/* Card1 */}
+              <Box sx={{ mb: 1 }}>
+                <Typography level="title-md">Make Request Form</Typography>
+              </Box>
+              <Divider />
+              <Stack
+                direction="row"
+                spacing={3}
+                sx={{ display: { xs: 'flex', md: 'flex' }, my: 1 }}
+              >
+
+                <Stack spacing={2} sx={{ flexGrow: 1 }}>
+                  <Stack spacing={1}>
+
+                    <FormLabel>Coal to be collected</FormLabel>
+                    <FormControl required
+                      sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}
+                    >
+                      <Input size="sm" placeholder="Coal to be collected" name="coalToBeCollected" />
+                    </FormControl>
+
+                    <FormLabel>Date</FormLabel>
+                    <FormControl required
+                      sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}
+                    >
+                      <Input size="sm" placeholder="Date" name="date" />
+                    </FormControl>
+
+                    <FormLabel>Time</FormLabel>
+                    <FormControl required
+                      sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}
+                    >
+                      <Input size="sm" placeholder="Time" name="time" />
+                    </FormControl>
+
+                    <FormLabel>Destination Station</FormLabel>
+                    <FormControl required
+                      sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}
+                    >
+                      <Input size="sm" placeholder="Destination Station" name="destinationStation" />
+                    </FormControl>
+                  </Stack>
+                </Stack>
+              </Stack>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              {/* SAVE AND CANCEL BUTTONS */}
+
+
+
+              <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+                <Button size="sm" variant="outlined" color="neutral" onClick={() => {
+                  console.log("hi");
+                  navigate('/dashboard/manage-siding');
+                }}>
+                  Cancel
+                </Button>
+                <Button size="sm" variant="solid" type="submit">
+                  Save
+                </Button>
+              </CardActions>
+
+            </Card>
 
 
 
           </Box>
-
-          {/* The main content of the page */}
-
-          {showForm && <OrderTable />}
-         
-          {
-            showAddForm && <AddForm />
-          }
-
-          {
-            showUpdateForm && <UpdateForm />
-          }
-              
-
-          {
-            showDeleteForm && <DeleteForm />
-          }  
-
-          
-          {/* <OrderList /> */}
-
-          {/* <MyProfile /> */}
-
-
 
 
 
         </Box>
 
-          
-
-      </Box>
-
-
-      
-
-
-    </CssVarsProvider>
 
 
 
 
+      </CssVarsProvider>
     </>
 
 
